f4bb46cdcc75ab7cc1cd532f33fbf441a5195348
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2023-04-15 21:37:10 +0000

    update

--- wifilm_front/src/components/search.vue
+++ wifilm_front/src/components/search.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="h-screen w-screen fixed z-40 shadow-md">
-        <div class="bg-zinc-900 flex flex-col items-center pt-10 pb-10 mx-20 rounded-md">
+        <div class="bg-zinc-900 flex flex-col items-center py-20 mx-20 rounded-md max-sm:py-10 max-sm:mx-5">
+            <div @click="close" class="flex w-full justify-end mr-5">
+                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 text-white cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </div>
             <div class="w-full px-10 border-none ">
                 <input type="text" class="w-full text-white py-2 px-2 border-b-2 bg-transparent focus:outline-none"
                     v-model="Search" placeholder="Search" />
             </div>
-            <div class="flex flex-col px-10 justify-start w-full ">
+            <div class="flex flex-col px-10 justify-start w-full">
                 <RouterLink v-for="suggestion in suggestions" :to="{ name: 'movie', params: { id: suggestion.id } }">
-                    <div class="flex py-5 justify-start items-center border-b-2 hover:bg-zinc-800">
+                    <div class="flex py-5 justify-start items-center border-b-2 hover:bg-zinc-800 max-sm:justify-center">
                         <img :src="suggestion.image" class="w-20 h-20  object-cover" alt="">
-                        <p class="pl-10 text-white">
+                        <p class="pl-10 text-white max-sm:hidden">
                             {{ suggestion.name }}
                         </p>
                     </div>
@@ -29,6 +35,9 @@ export default {
         }
     },
     methods: {
+        close(){
+            this.$parent.divVisible = false;
+        },
         getMovies() {
             axios.get("http://127.0.0.1:8000/api/searshformovies/" + this.Search)
                 .then(response => {
--- wifilm_front/src/views/MoviesView.vue
+++ wifilm_front/src/views/MoviesView.vue
@@ -1,11 +1,13 @@
 <template>
     <search v-if="divVisible" />
+
     <div class="overflow-x-auto mx-5">
         <div class="flex w-max my-5">
             <p :class="
                 [
                     'mx-2',
                     'px-5',
+                    'cursor-pointer',
                     'py-2',
                     'border',
                     'rounded-full',
@@ -18,6 +20,7 @@
                     'mx-2',
                     'px-5',
                     'py-2',
+                    'cursor-pointer',
                     'border',
                     'rounded-full',
                     { 'text-yellow-400': SelectedGenre === genre.id },
@@ -25,6 +28,15 @@
                 ]
             ">{{ genre.name }}</p>
         </div>
+
+    </div>
+    <div @click="opensearsh"
+        class="flex justify-end mr-10">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6 text-white cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
     </div>
     <div class="h-screen">
         <div class="flex flex-wrap justify-center">
@@ -64,6 +76,9 @@ export default {
         window.addEventListener('keydown', this.handleKeyDown)
     },
     methods: {
+        opensearsh() {
+            this.divVisible = !this.divVisible
+        },
         handleKeyDown(event) {
             if (event.ctrlKey && event.key === 'q') {
                 this.divVisible = !this.divVisible
